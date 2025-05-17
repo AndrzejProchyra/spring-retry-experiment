9404245fec184854a9950536934c205247a13f82
Andrzej Prochyra <andrzej@prochyra.io>
2025-05-17 16:54:41 +0100

    initial commit

---
+++ src/main/java/io/prochyra/springretryexperiment/SpringRetryExperimentApplication.java
@@ -0,0 +1,13 @@
+package io.prochyra.springretryexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRetryExperimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRetryExperimentApplication.class, args);
+    }
+
+}
